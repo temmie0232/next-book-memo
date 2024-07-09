9289c4e22e0532abe28507affc63979a7a66322f
temmie0232 <temmie0232@gmail.com>
2024-07-10 01:34:41 +0900

    feat: UserData、AuthFunctionsインターフェースとFirebaseUserタイプのエクスポート追加

---
+++ src/types/auth.types.ts
@@ -0,0 +1,14 @@
+import { User as FirebaseUser } from 'firebase/auth';
+
+export interface UserData {
+    email: string | null;
+    displayName: string | null;
+    initialized: boolean;
+}
+
+export interface AuthFunctions {
+    signUp: (email: string, password: string) => Promise<FirebaseUser>;
+    signIn: (email: string, password: string) => Promise<FirebaseUser>;
+}
+
+export type { FirebaseUser };
