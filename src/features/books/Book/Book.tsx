e01afe377eb31a4ab98ad51510f573fc0c4abe86
temmie0232 <temmie0232@gmail.com>
2024-07-10 20:53:15 +0900

    feat: 書籍コンポーネントの機能拡張
    ・Book コンポーネントの表示を更新し、タイトルとともに評価アイコンを表示
    ・書籍の評価を表示するアイコンを追加
    ・書籍のステータスに応じて色分けを実装
    ・getRatingIcon 関数を追加して評価に基づくアイコン選択を実装
    ・getStatusColor 関数を追加してステータスに基づく色選択を実装

--- src/features/books/Book/Book.tsx
+++ src/features/books/Book/Book.tsx
@@ -1,18 +1,53 @@
 import React from 'react';
 import Link from 'next/link';
-import { BookType } from '@/types/book.types';
+import { Book as BookType } from '@/types/book.types';
 import styles from './Book.module.css';
+import {
+    FaRegFaceGrinBeam,
+    FaRegFaceSmile,
+    FaRegFaceMeh,
+    FaRegFaceFrownOpen,
+    FaRegFaceFrown,
+    FaRegCircle
+} from 'react-icons/fa6';
 
 interface BookProps {
     book: BookType;
 }
 
-const Book: React.FC<BookProps> = ({ book }) => (
-    <Link href={`/book/${book.id}`} className={styles.bookLink}>
-        <div className={styles.book}>
-            <span className={styles.bookTitle}>{book.title}</span>
-        </div>
-    </Link>
-);
+const Book: React.FC<BookProps> = ({ book }) => {
+    // 評価に基づいてアイコンを選択する関数
+    const getRatingIcon = (rating: string | null) => {
+        switch (rating) {
+            case '5': return <FaRegFaceGrinBeam />;
+            case '4': return <FaRegFaceSmile />;
+            case '3': return <FaRegFaceMeh />;
+            case '2': return <FaRegFaceFrownOpen />;
+            case '1': return <FaRegFaceFrown />;
+            default: return <FaRegCircle />;
+        }
+    };
+
+    // ステータスに基づいて色を選択する関数
+    const getStatusColor = (status: 'not-started' | 'in-progress' | 'completed') => {
+        switch (status) {
+            case 'not-started': return 'text-red-500';
+            case 'in-progress': return 'text-yellow-500';
+            case 'completed': return 'text-green-500';
+            default: return '';
+        }
+    };
+
+    return (
+        <Link href={`/book/${book.id}`} className={styles.bookLink}>
+            <div className={styles.book}>
+                <span className={styles.bookTitle}>{book.title}</span>
+                <div className={`${styles.bookStatus} ${getStatusColor(book.status)}`}>
+                    {getRatingIcon(book.rating)}
+                </div>
+            </div>
+        </Link>
+    );
+};
 
 export default Book;
