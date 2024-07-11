7fc2e8e80b33ed93df93930a554e6cf85cf777bf
temmie0232 <temmie0232@gmail.com>
2024-07-12 01:33:07 +0900

    update: 設定を別コンポーネントに分離して、そのコンポーネントを使用

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import styles from "./header.module.css";
-import { MdDarkMode, MdLightMode } from "react-icons/md";
-import { IoIosLogOut, IoMdSettings } from "react-icons/io";
-import { FaSearch, FaUser } from "react-icons/fa";
+import { FaSearch } from "react-icons/fa";
 import Image from 'next/image';
 import logo_light from "~/public/logo-black.png";
 import logo_dark from "~/public/logo-white.png";
@@ -10,28 +8,16 @@ import { auth } from '~/firebase';
 import { useTheme } from '@/contexts/theme/useTheme';
 import { signOut } from 'firebase/auth';
 import { useRouter } from 'next/navigation';
-import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
-    DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import CustomTooltip from '@/components/elements/CustomTooltip';
+import SettingsDropdown from '@/components/elements/SettingsDropdown';
 
 interface HeaderProps {
     onSearch: (searchTerm: string) => void;
 }
 
 const Header: React.FC<HeaderProps> = ({ onSearch }) => {
-    const { theme, setTheme } = useTheme();
+    const { theme } = useTheme();
     const router = useRouter();
     const [searchTerm, setSearchTerm] = useState('');
-
-    const toggle_mode = () => {
-        setTheme(theme === "light" ? "dark" : "light");
-    };
 
     const handleLogout = async () => {
         try {
@@ -72,49 +58,7 @@ const Header: React.FC<HeaderProps> = ({ onSearch }) => {
                 </button>
             </form>
 
-            <DropdownMenu>
-                <CustomTooltip content="設定">
-                    <DropdownMenuTrigger className={` ${styles.settingsButton} `}>
-                        <IoMdSettings className="w-9 h-9" />
-                    </DropdownMenuTrigger>
-                </CustomTooltip>
-                <DropdownMenuContent>
-                    <DropdownMenuLabel>
-                        <div className={styles.userInfo}>
-                            {auth.currentUser?.photoURL ? (
-                                <Image
-                                    src={auth.currentUser.photoURL}
-                                    alt="User"
-                                    width={32}
-                                    height={32}
-                                    className={styles.userAvatar}
-                                />
-                            ) : (
-                                <FaUser className={styles.userIcon} />
-                            )}
-                            <span className={styles.displayName}>{auth.currentUser?.displayName}</span>
-                        </div>
-                    </DropdownMenuLabel>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuItem onClick={toggle_mode} className={styles.dropdownItem}>
-                        {theme === "light" ? (
-                            <>
-                                <MdDarkMode className={styles.menuIcon} />
-                                <span>ダークモードに切り替え</span>
-                            </>
-                        ) : (
-                            <>
-                                <MdLightMode className={styles.menuIcon} />
-                                <span>ライトモードに切り替え</span>
-                            </>
-                        )}
-                    </DropdownMenuItem>
-                    <DropdownMenuItem onClick={handleLogout} className={styles.dropdownItem}>
-                        <IoIosLogOut className={styles.menuIcon} />
-                        <span>ログアウト</span>
-                    </DropdownMenuItem>
-                </DropdownMenuContent>
-            </DropdownMenu>
+            <SettingsDropdown onLogout={handleLogout} />
         </div>
     );
 };
