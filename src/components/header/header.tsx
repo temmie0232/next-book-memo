13be685f636c257334170ca03c66b3c6ead7df9d
temmie0232 <temmie0232@gmail.com>
2024-07-11 12:14:10 +0900

    update: ボタンのスタイルの修正

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -73,7 +73,7 @@ const Header: React.FC<HeaderProps> = ({ onSearch }) => {
 
             <DropdownMenu>
                 <DropdownMenuTrigger className={` ${styles.settingsButton} `}>
-                    <IoMdSettings className="w-9 h-9 border-none" />
+                    <IoMdSettings className="w-9 h-9" />
                 </DropdownMenuTrigger>
                 <DropdownMenuContent>
                     <DropdownMenuLabel>
